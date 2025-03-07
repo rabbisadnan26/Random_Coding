46c1bff574883dd3567358876bfb077fba400f42
rabbisadnan26 <rabbisadnankhan@gmail.com>
2025-03-08 01:05:35 +0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+##Code 1 
+s = "Bob"
+print(s)
+s = "Yaad"
+year = 2000
+city = "Bangladesh"
+print(s,year,city)
+
+##Code 2
+x,y = input("Enter two values: ").split()
+print(f"Number of boys and girls are: {x} and {y} ")
+
+x,y,z = input("Enter three values: ").split()
+print(f"Number of their siblings are: {x} and {y} ")
---
+++ 2.py
@@ -0,0 +1,25 @@
+#Age 
+
+age_input = input("Enter your age brother: ")
+age = int(age_input)
+
+if age < 0:
+    print("You are not even born bro. ")
+elif age < 18:
+    print("Rektu boro ho ")
+elif age >= 18 and age < 65 :
+    print("Adult bro tumi ")
+else:
+    print("Borooooooo Bhai")
+    
+    # Print numbers in python
+
+n = int(input("Enter a no: "))
+print(n) 
+
+c = float(input("Enter a no: "))
+print(n)
+
+print(type(n))
+print(type(c))
+
